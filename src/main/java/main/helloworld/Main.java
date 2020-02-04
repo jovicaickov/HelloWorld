0d07d99ec38152703e91c0bc4c5f884afbdbda7e
Jovica Ickov <jovica.ickov@gmail.com>
2020-02-04 18:44:36 +0100

    Initial commit

---
+++ src/main/java/main/helloworld/Main.java
@@ -0,0 +1,17 @@
+package main.helloworld;
+
+/**
+ *
+ * @author qa
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Jovica Ickov");
+    }
+    
+}
