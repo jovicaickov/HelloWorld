6ef87995de77bf339bcf806e317ce814fa37e087
Jovica Ickov <jovica.ickov@gmail.com>
2020-02-04 20:23:44 +0100

    Adds age checker

--- src/main/java/main/helloworld/Main.java
+++ src/main/java/main/helloworld/Main.java
@@ -1,5 +1,6 @@
 package main.helloworld;
 
+import java.util.Scanner;
 /**
  *
  * @author qa
@@ -12,6 +13,33 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Jovica Ickov");
+        
+        int age = 5;
+        
+        if (age > 18) {
+           System.out.println("Osoba je punoletna.");
+        } else {
+           System.out.println("Osoba je maloletna.");
+        }
+        
+        System.out.print("Unesite broj godina: ");
+        Scanner input = new Scanner(System.in);
+        int userAge = input.nextInt();
+        
+        System.out.println("Korisnik je uneo " + userAge);
+        
+        if (userAge <= 0 && userAge > 150) {
+           System.out.println("Osoba je beba.");
+        } else if (userAge <= 5) {
+           System.out.println("Osoba je dete.");
+        } else if (userAge <= 11) {
+           System.out.println("Osoba je dete.");
+        } else if (userAge <= 17) {
+           System.out.println("Osoba je tinejdzer.");
+        } else if (userAge >= 18) {
+           System.out.println("Osoba je odrasla.");
+        }
+        
     }
     
 }
