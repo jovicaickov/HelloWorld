4dd79176c3d88ea5b6fe06fe77cd7af00701ba09
Jovica Ickov <jovica.ickov@gmail.com>
2020-02-06 19:59:06 +0100

    adds examples for different loop types

--- src/main/java/main/helloworld/Main.java
+++ src/main/java/main/helloworld/Main.java
@@ -14,32 +14,61 @@ public class Main {
         System.out.println("Hello World");
         System.out.println("Jovica Ickov");
         
-        int age = 5;
-        
-        if (age > 18) {
-           System.out.println("Osoba je punoletna.");
-        } else {
-           System.out.println("Osoba je maloletna.");
-        }
-        
-        System.out.print("Unesite broj godina: ");
-        Scanner input = new Scanner(System.in);
-        int userAge = input.nextInt();
-        
-        System.out.println("Korisnik je uneo " + userAge);
-        
-        if (userAge <= 0 && userAge > 150) {
-           System.out.println("Osoba je beba.");
-        } else if (userAge <= 5) {
-           System.out.println("Osoba je dete.");
-        } else if (userAge <= 11) {
-           System.out.println("Osoba je dete.");
-        } else if (userAge <= 17) {
-           System.out.println("Osoba je tinejdzer.");
-        } else if (userAge >= 18) {
-           System.out.println("Osoba je odrasla.");
-        }
-        
-    }
+//        int age = 5;
+//        
+//        if (age > 18) {
+//           System.out.println("Osoba je punoletna.");
+//        } else {
+//           System.out.println("Osoba je maloletna.");
+//        }
+//        
+//        System.out.print("Unesite broj godina: ");
+//        Scanner input = new Scanner(System.in);
+//        int userAge = input.nextInt();
+//        
+//        System.out.println("Korisnik je uneo " + userAge);
+//        
+//        if (userAge <= 0 && userAge > 150) {
+//           System.out.println("Osoba je beba.");
+//        } else if (userAge <= 5) {
+//           System.out.println("Osoba je dete.");
+//        } else if (userAge <= 11) {
+//           System.out.println("Osoba je dete.");
+//        } else if (userAge <= 17) {
+//           System.out.println("Osoba je tinejdzer.");
+//        } else if (userAge >= 18) {
+//           System.out.println("Osoba je odrasla.");
+//        }
+//        
+//        System.out.println("While loop:");
+//        
+//        int x = 5;
+//        while(x < 10) {
+//            System.out.println(x);
+//            x = x + 1;
+//        }           
     
-}
+//        for (int i = 30; i >= 20; i--) {
+//            System.out.println(i);
+//        }
+    
+
+//          for (int i = 0; i < 10; i++) {
+//              for (int j = 0; j < 10; j++) {
+//                  System.out.print("*");
+//              } 
+//            System.out.println();
+//            }
+
+          
+          for (int i = 1; i < 11; i++) {
+              for (int j = 1; j < 11; j++) {
+                  System.out.print("(" + i + "," + j + ") ");
+              } 
+            System.out.println();
+            }
+            
+            }
+          
+          
+    }
